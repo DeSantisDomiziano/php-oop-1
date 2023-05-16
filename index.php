01a1188b5bef58f94f807390e5d3e609a3a589a1
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-05-16 16:03:52 +0200

    add method(function)

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ class Info {
     public $cast;
     public $language;
 
-    public function __construct(string $director, string $cast, string $language) {
+    public function __construct(string $director, string $cast, string $language = 'english') {
         $this->director = $director;
         $this->cast = $cast;
         $this->language = $language;
@@ -17,16 +17,24 @@ class Movie {
     public $overview;
     public $kind;
     public $info;
+    public $vote;
     
-    public function __construct(string $title, string $overview, string $kind, Info $info) {
+    public function __construct(string $title, string $overview, string $kind, Info $info, int $vote) {
         $this->title = $title;
         $this->overview = $overview;    
         $this->kind = $kind;
         $this->info = $info;
+        $this->vote = $vote;
+    }
+
+    public function changeVote($newVote) {
+        $this->vote = $newVote;
     }
 }
 
-$avatar = new Movie('Avatar', 'lorem' , 'fantasi', new Info('lorem', 'lorem', 'lorem'));
+$avatar = new Movie('Avatar', 'lorem' , 'fantasy', new Info('lorem', 'lorem', 'Italian'), 6);
+
+$avatar->changeVote(10);
 
 var_dump($avatar)
 ?>
