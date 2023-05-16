c6ffaa5a8d5ec21babf5f44d70b9b6aff5efd8fd
DeSantisDomiziano <desantis.domiziano@gmail.com>
2023-05-16 15:43:36 +0200

    add composition

--- index.php
+++ index.php
@@ -1,18 +1,32 @@
 <?php 
 
+class Info {
+    public $director;
+    public $cast;
+    public $language;
+
+    public function __construct(string $director, string $cast, string $language) {
+        $this->director = $director;
+        $this->cast = $cast;
+        $this->language = $language;
+    }
+}
+
 class Movie {
     public $title;
     public $overview;
     public $kind;
-
-    public function __construct(string $title, string $overview, string $kind) {
+    public $info;
+    
+    public function __construct(string $title, string $overview, string $kind, Info $info) {
         $this->title = $title;
         $this->overview = $overview;    
         $this->kind = $kind;
+        $this->info = $info;
     }
 }
 
-$avatar = new Movie('Avatar', 'lorem' , 'fantasi');
+$avatar = new Movie('Avatar', 'lorem' , 'fantasi', new Info('lorem', 'lorem', 'lorem'));
 
 var_dump($avatar)
 ?>
